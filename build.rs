65b21b9a16be4acd16665ab09288815645c8e429
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2024-01-17 23:33:48 +0100

    Fix builds failing on Linux

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
-use winres::WindowsResource;
-use winapi::um::winnt;
-
 fn main() {
     if cfg!(target_os = "windows") {
+        use winres::WindowsResource;
+        use winapi::um::winnt;
+
         WindowsResource::new()
             .set_icon("assets/icon.ico")
             .set_language(winnt::MAKELANGID(winnt::LANG_ENGLISH, winnt::SUBLANG_ENGLISH_US))
