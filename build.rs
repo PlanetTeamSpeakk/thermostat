70baf3240775e47e0b2d5d7494ffeb6378b71ed3
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2024-01-20 21:19:54 +0100

    Improve build script

--- build.rs
+++ build.rs
@@ -1,14 +1,8 @@
 fn main() {
-    compile_windows();
-
-    slint_build::compile("ui/logic.slint").unwrap();
-    slint_build::compile("ui/elements.slint").unwrap();
-    slint_build::compile("ui/appwindow.slint").unwrap();
-}
-
-#[cfg(target_os = "windows")]
-fn compile_windows() {
-    use winres::WindowsResource;
+    #[cfg(target_os = "windows")]
+    {
+        // Windows-specific extras
+        use winres::WindowsResource;
         use winapi::um::winnt;
 
         WindowsResource::new()
@@ -16,7 +10,7 @@ fn compile_windows() {
             .set_language(winnt::MAKELANGID(winnt::LANG_ENGLISH, winnt::SUBLANG_ENGLISH_US))
             .compile()
             .unwrap();
-}
+    }
 
-#[cfg(not(target_os = "windows"))]
-fn compile_windows() {} // No-op on non-Windows platforms
+    slint_build::compile("ui/appwindow.slint").unwrap();
+}
